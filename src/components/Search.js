da034e5142bab51adb62c83940a7c381804558e8
Ruhul Amin <ruhulinfo50@gmail.com>
2020-12-01 00:33:28 +0600

    imp search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,27 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Search() {
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const resData = await axios("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(resData.data.query.search);
+    };
+    if (term) {
+      search();
+    }
+  }, [term]);
+
+  const renderElement = results.map((result) => {
+    return (
+      <div key={result.pageid}>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <h5>{result.title}</h5>
+            <p>{result.snippet}</p>
+            <a
+              className="btn btn-primary"
+              href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            >
+              GO
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  });
+
   return (
     <div>
       <div className="input-group flex-nowrap">
         <input
+          onChange={(e) => setTerm(e.target.value)}
           type="text"
           className="form-control"
           placeholder="Enter Search Term"
           aria-describedby="addon-wrapping"
+          value={term}
         />
       </div>
       <br />
-      <ul className="list-group">
-        <li className="list-group-item">
-          <h5>Programming</h5>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. At dolore
-            ducimus repellat?
-          </p>
-          <button className="btn btn-primary">GO</button>
-        </li>
-      </ul>
+      {renderElement}
     </div>
   );
 }
